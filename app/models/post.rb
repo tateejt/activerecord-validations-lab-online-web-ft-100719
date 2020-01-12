9394932f42cd57a0198246af57707898b8eeb099
Emery Tate <tateejt@gmail.com>
2020-01-12 02:04:55 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true 
+  validates(:content, { :length => { :minimum => 250 } })
+  validates(:summary, { :length => { :maximum => 250 } })
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :clickbait
+  
+  def clickbait
+    if category == "Won't Belive" | "Secret" | "Top 3" | "Guess"
+      errors.add(:category, "can't be clickbait-y")
+    end
+  end
 end
